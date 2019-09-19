1eda215f8bb6c4bd6798c5b8a07c3e305dccb6bb
Ralf Ebert <info@ralfebert.de>
2019-09-19 11:07:52 +0200

    grouping by sections

--- NewspaperExample/StoriesTableViewController.swift
+++ NewspaperExample/StoriesTableViewController.swift
@@ -8,6 +8,26 @@ struct Headline {
     var image : String
 }
 
+private func firstDayOfMonth(date: Date) -> Date {
+    let calendar = Calendar.current
+    let components = calendar.dateComponents([.year, .month], from: date)
+    return calendar.date(from: components)!
+}
+
+struct MonthSection {
+
+    var month : Date
+    var headlines : [Headline]
+
+    static func group(headlines : [Headline]) -> [MonthSection] {
+        let groups = Dictionary(grouping: headlines) { headline in
+            firstDayOfMonth(date: headline.date)
+        }
+        return groups.map(MonthSection.init(month:headlines:))
+    }
+
+}
+
 private func parseDate(_ str : String) -> Date {
     let dateFormat = DateFormatter()
     dateFormat.dateFormat = "yyyy-MM-dd"
@@ -23,22 +43,42 @@ class StoriesTableViewController: UITableViewController {
         Headline(id: 4, date: parseDate("2018-02-10"), title: "Aenean condimentum", text: "Ut eget massa erat. Morbi mauris diam, vulputate at luctus non, finibus et diam. Morbi et felis a lacus pharetra blandit.", image: "Banana"),
     ]
 
+    var sections = [MonthSection]()
+
     // MARK: - View Controller lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.sections = MonthSection.group(headlines: self.headlines)
+        self.sections.sort { lhs, rhs in lhs.month < rhs.month }
+
     }
 
     // MARK: - UITableViewDataSource
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.sections.count
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let section = self.sections[section]
+        let date = section.month
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM yyyy"
+        return dateFormatter.string(from: date)
+    }
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.headlines.count
+        let section = self.sections[section]
+        return section.headlines.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
 
-        let headline = self.headlines[indexPath.row]
+        let section = self.sections[indexPath.section]
+        let headline = section.headlines[indexPath.row]
         cell.textLabel?.text = headline.title
         cell.detailTextLabel?.text = headline.text
         cell.imageView?.image = UIImage(named: headline.image)
